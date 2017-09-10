9ded2e5ba66d52d88b2cad939d0f9fd10c7dcf58
Ben Wiley <ben.wiley@autodesk.com>
2017-09-09 20:32:42 -0400

    First draft of Sierpinski Gasket program computes and prints 5000 points.

---
+++ sierpinski_gasket.cpp
@@ -0,0 +1,27 @@
+#include "angel/Angel.h"
+#include <stdlib.h>
+
+typedef vec2 point2;
+
+int main()
+{
+  const int NumPoints = 5000;
+  point2 points[NumPoints];
+
+  // triangle in plane z = 0
+  point2 vertices[3] = {point2(-1.0,-1.0), point2(0.0,1.0), point2(1.0,-1.0)};
+
+  // arbitrary initial point
+  points[0] = point2(0.25, 0.50);
+
+  // add NumPoints - 1 new points
+
+  for (int k = 0; k < NumPoints; k++) {
+    // pick vertex at random
+    int j = rand() % 3;
+
+    // Compute halfway point between selected vertex and last point
+    points[k] = (points[k-1] + vertices[j]) / 2.0;
+    std::cout << points[k] << std::endl;
+  }
+}
