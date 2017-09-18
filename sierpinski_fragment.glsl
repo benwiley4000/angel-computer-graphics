940397eefefdbf14a1c9e3a6c6c5e4c5cdaa26f2
Ben Wiley <therealbenwiley@gmail.com>
2017-09-17 22:12:03 -0400

    Shaders color by position.

--- sierpinski_fragment.glsl
+++ sierpinski_fragment.glsl
@@ -1,4 +1,8 @@
+#version 130
+
+in vec4 color;
+
 void main()
 {
-  gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);
+  gl_FragColor = color;
 }
--- sierpinski_vertex.glsl
+++ sierpinski_vertex.glsl
@@ -1,6 +1,10 @@
-attribute vec4 vPosition;
+#version 130
+
+in vec4 vPosition;
+out vec4 color;
 
 void main()
 {
+  color = vec4((1.0 + vPosition.xyz) / 2.0, 1.0);
   gl_Position = vPosition;
 }
